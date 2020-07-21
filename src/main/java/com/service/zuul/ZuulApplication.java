d08eb74872adfb697005d566339dfe12cc62835f
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-07-21 16:26:23 +0800

    INIT

---
+++ src/main/java/com/service/zuul/ZuulApplication.java
@@ -0,0 +1,26 @@
+package com.service.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author : pc
+ * @date : 2020/7/17
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ZuulApplication.class);
+    }
+}
